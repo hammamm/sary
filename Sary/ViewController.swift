5639f9f7ff98278cc0eb5b0de5b7bcca169af024
hammam <hammam.a@munjz.com>
2021-09-15 08:49:48 +0300

    Initial Commit

---
+++ Sary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sary
+//
+//  Created by hammam abdulaziz on 08/02/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
